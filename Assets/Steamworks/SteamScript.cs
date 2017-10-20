6013cecbdd00406f33961b16b8d9b40dd725a958
Mkbae <baemingi@BaeMinGiui-iMac.local>
2017-10-20 11:54:45 +0900

    save

---
+++ Assets/Steamworks/SteamScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Steamworks;
+
+public class SteamScript : MonoBehaviour
+{
+	private void Start()
+	{
+		string n = SteamFriends.GetPersonaName();
+		Debug.Log(n);
+	}
+}
